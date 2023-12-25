2d36bf236971d7a7b70fa64c9e4bbba784ac9b94
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2023-12-26 00:51:30 +0700

    Delete shop directory

--- shop/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from shop.models import Product
-
-
-# Create your views here.
-class ProductsListView(ListView):
-    model = Product
-    template_name = 'shop/product_list.html'
